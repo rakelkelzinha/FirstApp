77446ef3ef99d2bbe0f80e402784882105ec662a
Rakel Laiane Oliveira da Silva <rakel.silva@portalsesisp.org.br>
2024-03-05 11:35:28 -0300

    criar um layout para calcular a idade.
    criar um layout para validar um email e um telefone.

--- app/src/main/java/com/rakel/firstapp/MainActivity.kt
+++ app/src/main/java/com/rakel/firstapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.rakel.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rakel.firstapp.databinding.ActivityMainBinding
+import com.rakel.firstapp.databinding.TelaLinearBinding
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEnviar.setOnClickListener{
+            var nome = binding.edtNome.editableText.toString()
+
+            binding.tdNome.text ="Nome:" + nome
+
+            var anoNascimento = binding.edtAnoNascimento.editableText.toString()
+            val anoAtual = LocalDateTime.now().year
+            var idade = anoAtual - anoNascimento.toInt()
+
+            binding.tdIdade.text = "Idade: ${idade}"
+
+
+        }
+        binding.btnEnviar.setOnClickListener{
+            var email = binding.edtEmail.editableText.toString()
+
+            var telefone = binding.edtTelefone.editableText.toString()
+
+
+            if(email.contains("@") && (email.contains(".com"))){
+                binding.tvEmail.text = "Email valido:" + email
+            }else{
+                binding.tvEmail.text="Email invalido:"
+            }
+            if(telefone.length == 11){
+                binding.tvTel.text= "telefone valido:" + telefone
+            }else{
+                binding.tvTel.text= "telefone invalido :"
+            }
+        }
     }
 }
